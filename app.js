e7366b8db384768c0272ebe7efe337968756c0e9
Won D <fkiller@gmail.com>
2020-08-30 22:24:19 -0400

    New function - Added list recent tracks from my followed artists

--- app.js
+++ app.js
@@ -174,8 +174,8 @@
 
 
 	if (location.host == 'localhost:8000') {
-		client_id = 'd37a9e88667b4fb3bc994299de2a52bd';
-		redirect_uri = 'http://localhost:8000/callback.html';
+		client_id = '7aaea0cce0844148aafadd895675ac8d';
+		redirect_uri = `http://${location.host}/callback.html`;
 	} else {
 		client_id = '6f9391eff32647baa44b1a700ad4a7fc';
 		redirect_uri = 'http://lab.possan.se/playlistcreator-example/callback.html';
@@ -184,7 +184,7 @@
 	var doLogin = function(callback) {
 		var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
 			'&response_type=token' +
-			'&scope=playlist-read-private%20playlist-modify%20playlist-modify-private' +
+			'&scope=playlist-read-private%20playlist-modify%20playlist-modify-private%20user-follow-read%20user-follow-modify' +
 			'&redirect_uri=' + encodeURIComponent(redirect_uri);
 		localStorage.setItem('createplaylist-tracks', JSON.stringify(g_tracks));
 		localStorage.setItem('createplaylist-name', g_name);
@@ -212,7 +212,10 @@
 		});
 		$('#start').click(function() {
 			doLogin(function() {});
-		})
+		});
+		$('#login').click(function() {
+			doLogin(function() {});
+		});
 		$('#alltext').text('hello world');
 		refreshText();
 		resolveOneWord();
